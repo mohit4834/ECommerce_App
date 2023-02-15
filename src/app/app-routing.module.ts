cd645d4b4b3129d798453aa83f84aebc9a660786
mohitgoyal48 <mohit.goyal@nagarro.com>
2023-02-15 11:12:29 +0530

    routing and env files added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ExtUrlResolverService } from './ext-url-resolver.service';
+import { FallBackComponent } from './fall-back/fall-back.component';
+import { GitAuthComponent } from './git-auth/git-auth.component';
+import { LoginComponent } from './login/login.component';
+import { RedirectComponent } from './redirect/redirect.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    pathMatch:"full",
+    redirectTo:'login'
+    },
+    {
+    path:'dashboard',
+    component:DashboardComponent
+    },
+    {
+    path:'login',
+    component:LoginComponent
+    },
+    {
+    path:'redirect',
+    component:RedirectComponent
+    },
+    {
+    path: 'test',
+    component: GitAuthComponent,
+    resolve: {
+    url: ExtUrlResolverService
+    }
+    },
+    {
+    path:'**',
+    component:FallBackComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { RedirectComponent } from './redirect/redirect.component';
 import { FallBackComponent } from './fall-back/fall-back.component';
+import { GitAuthComponent } from './git-auth/git-auth.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +15,14 @@ import { FallBackComponent } from './fall-back/fall-back.component';
     DashboardComponent,
     LoginComponent,
     RedirectComponent,
-    FallBackComponent
+    FallBackComponent,
+    GitAuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientXsrfModule
   ],
   providers: [],
   bootstrap: [AppComponent]
